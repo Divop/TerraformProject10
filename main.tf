5bb1d9abac26d9bd58efa984df4ff1b096affef1
Divop <93809648+Divop@users.noreply.github.com>
2022-09-19 17:22:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-	region = "us-west-1"
-}
+#provider "aws" {
+#	region = "us-west-1"
+#}
 
-resource "aws_instance" "Websever" {
-	ami = "ami-085284d24fe829cd0"
-	instance_type = "t2.micro"
-}
+#resource "aws_instance" "Websever" {
+#	ami = "ami-085284d24fe829cd0"
+#	instance_type = "t2.micro"
+#}
 
-resource "aws_s3_bucket" "Dinobucket" {
-	bucket = "dino0101"
-	acl = "private"
-}
+#resource "aws_s3_bucket" "Dinobucket" {
+#	bucket = "dino0101"
+#	acl = "private"
+#}
